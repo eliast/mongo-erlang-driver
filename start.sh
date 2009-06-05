6a9116997bff8a1a565f3e2a6b04031cc587d081
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2009-06-05 23:45:11 +0200

    Fix erl invocation in start script Signed-off-by: Tuncer Ayaz <tuncer.ayaz@gmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl +K true -config src/mongodb -s mongodb -sname mongo $@
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl +K true -config src/mongodb -sname mongo $@
