e1534eeb916fd06ecbc63ed03e971de503552760
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2009-07-09 02:33:58 +0200

    Clarify comments in startdev.sh
    Signed-off-by: Tuncer Ayaz <tuncer.ayaz@gmail.com>

--- startdev.sh
+++ startdev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
-# For the following to work make sure that reloader is in your local code
-# path (use code:load_abs/1 in ~/.erlang). We don't bundle it with the sources.
+# For the following to work make sure that reloader is in the code
+# path (e.g., use code:load_abs/1 or code:add_pathsz/1 in ~/.erlang).
+# You can obtain it from mochiweb's sources.
 exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl +K true -config src/mongodb -sname mongo -run reloader start $@
